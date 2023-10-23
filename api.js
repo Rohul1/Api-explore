57faa01133dfdc330af0e9c26dd8ae07927d4b35
Rohul1 <rohul669319@gmail.com>
2023-10-23 21:54:32 +0600

    first commit

---
+++ api.js
@@ -0,0 +1,29 @@
+// const sheild = {
+//     name : "rohul",
+//     age : 22,
+//     roll : 16,
+//     batch : 2019
+// }
+// console.log(JSON.stringify(sheild));
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(Response => Response.json())
+.then(datas => {
+    
+for(const data of datas){
+console.log("person name:", data.name);
+console.log("company name:", data.company.name);
+
+}
+
+
+
+})
