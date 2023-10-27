d0ae4894925d1602f76099a3e0105e2f879e0f7f
Rohul1 <rohul669319@gmail.com>
2023-10-27 23:10:31 +0600

    added html

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ function loadUsers2 (){
     .then(data => displayUsers2(data))
 }
 function displayUsers2 (data){
-console.log(data)
+   const ul = document.getElementById("users-list");
+   for ( const user of data){
+    console.log(user.name);
+    const li = document.createElement("li");
+    li.innerText = user.name;
+    ul.appendChild(li);
+
+}
 }
