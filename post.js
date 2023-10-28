682b45ce4ee5944227315ab12b59c1313ffd1d97
Rohul1 <rohul669319@gmail.com>
2023-10-28 22:28:52 +0600

    added final

--- post.js
+++ post.js
@@ -8,11 +8,12 @@ function displayPost (posts){
     const postsContainer = document.getElementById("posts-container");
     for(const post of posts){
         const postDiv = document.createElement("div");
+        postDiv.classList.add("post")
         console.log(post);
         postDiv.innerHTML = `
         <h4>User-${post.userId}</h4>
         <h5>Post : ${post.title}</h5>
-        <p> post discription</p>
+        <p> post discription : ${post.body}</p>
         `;
         postsContainer.appendChild(postDiv);
     }
